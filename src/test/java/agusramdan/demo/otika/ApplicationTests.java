b481af4735b22419c9c6850adb7f53e5c12fdfc7
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2021-03-27 20:52:21 +0700

    track aps

---
+++ src/test/java/agusramdan/demo/otika/ApplicationTests.java
@@ -0,0 +1,16 @@
+package agusramdan.demo.otika;
+
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	 
+	public void contextLoads() {
+	}
+
+}
